b7afd8a22477d940a17e2e3303c26297a59d47a8
Mrilyes <ilyesghlila1@gmail.com>
2025-01-11 13:27:59 +0000

    Set Up the Test Project

---
+++ backend/OrderOrderline.Tests/ExampleTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+public class ExampleTests
+{
+    [Fact]
+    public void Test_Addition()
+    {
+        // Arrange
+        int a = 1;
+        int b = 1;
+
+        // Act
+        int result = a + b;
+
+        // Assert
+        Assert.Equal(2, result);
+    }
+}
