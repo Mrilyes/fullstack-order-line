75297bde20f715e8d3b64374a12383cbfa7f5908
Mrilyes <ilyesghlila1@gmail.com>
2025-01-14 21:01:37 +0000

    add eslint for cypress

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -10,10 +10,23 @@ export default [
 
   {
     name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/cypress/**'],
   },
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
   skipFormatting,
+  {
+    name: 'Cypress',
+    files: ['**/cypress/**/*.js'],
+    env: {
+      'cypress/globals': true, // Enables Cypress global variables like cy
+    },
+    extends: [
+      'plugin:cypress/recommended'
+    ],
+    rules: {
+      // You can specify rules specific to Cypress test files here
+    }
+  },
 ]
