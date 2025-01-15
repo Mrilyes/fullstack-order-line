df18c6db4626f099fcb724be339015c2f63653ac
Mrilyes <ilyesghlila1@gmail.com>
2025-01-15 09:51:28 +0000

    test the routes

--- frontend/cypress/e2e/router.cy.js
+++ frontend/cypress/e2e/router.cy.js
@@ -1,17 +1,17 @@
 describe('Router', () => {
-  it('Loads the OrderDashboardLayout component for the default route', () => {
+  it('Navigates to the Order Dashboard', () => {
     cy.visit('/'); // Visit the default route
+    cy.url().should('include', '/'); // Check if the URL is correct
 
-    // Check if the Navbar exists
-    cy.get('.navbar').should('exist');
+  });
 
-    // Check if the left panel exists
-    cy.get('.left-panel').should('exist');
+  it('Navigates to the Article Dashboard', () => {
+    cy.visit('/articledashboard'); // Visit the Article Dashboard route
+    cy.url().should('include', '/articledashboard'); // Check if the URL is correct
+  });
 
-    // Check if the middle panel exists
-    cy.get('.middle-panel').should('exist');
-
-    // Check if the right panel exists
-    cy.get('.right-panel').should('exist');
+  it('Navigates to the Home Page', () => {
+    cy.visit('/homePage'); // Visit the Home Page route
+    cy.url().should('include', '/homePage'); // Check if the URL is correct
   });
 });
